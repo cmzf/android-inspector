0d4ea021f0a4774fc3896b7bfcc82a48d6ee6349
cmzf <59588993+cmzf@users.noreply.github.com>
2020-01-19 13:10:14 +0800

    fix app name

--- app/src/main/java/com/github/cmzf/androidinspector/MainActivity.java
+++ app/src/main/java/com/github/cmzf/androidinspector/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.activity_name);
 
         Global.setMainActivity(this);
         Global.setMainApplication(this.getApplication());
